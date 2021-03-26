815f0c62036bd985a630cb7793d26f8694cbe5de
FabienGaubert <fab.gaubert@gmail.com>
2021-03-26 16:46:20 +0100

    implemented half of the instructions

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 
 #define MEMORY_SIZE 4096
 #define START_OF_PROGRAM 0x200
@@ -55,16 +56,16 @@ uint8_t fonts[FONTS_SIZE] =
 };
 
 uint8_t registers[REGISTER_COUNT];
-uint16_t PC=0x200;
+uint16_t PC = START_OF_PROGRAM;
 uint16_t I;
-uint8_t delayTimer=0;
-uint8_t soundTimer=0;
+uint8_t delayTimer = 0;
+uint8_t soundTimer = 0;
 
 // can be optimized with bools or bit operations
 unsigned char screen[SCREEN_HEIGHT][SCREEN_WIDTH];
 
 uint16_t stack[32];
-uint8_t SP=0x00;
+uint8_t SP=0;
 
 void printScreen(){
     for(int i=0; i<SCREEN_HEIGHT; i++){
@@ -129,70 +130,187 @@ int main(){
     loadFonts();
 
     //printScreen();
-    printRAM(0x200, numberBytes);
+    printRAM(START_OF_PROGRAM, numberBytes);
 
+    PC+=4;
 
-    uint16_t instruction=memory[PC];
-    switch(instruction&0xF000){
+    uint16_t instruction = ((memory[PC] << 8) | memory[PC+1]);
+    printf("Instruction: %.4x\n", instruction);
+    uint8_t x = (instruction >> 8) & 0x000F;
+    uint8_t y = (instruction >> 4) & 0x000F;
+
+    switch(instruction & 0xF000){
         case 0x0000:
         switch(instruction){
             case 0x00E0:
             clearScreen();
+            PC+=2;
             break;
+
+            case 0x00EE:
+            PC=stack[SP];
+            SP--;
+            break;
+
+            default:
+            printf("Instruction not handled: 0x%.4x\n", instruction);
         }
-        
         break;
 
         case 0x1000:
-        
+        PC = instruction & 0x0FFF;
         break;
 
         case 0x2000:
-        
+        stack[++SP] = PC;
+        PC = instruction & 0x0FFF;
         break;
 
         case 0x3000:
-        
+        if(registers[x] == instruction & 0x00FF){
+            PC+=4;
+        }
+        else{
+            PC+=2;
+        }
         break;
 
         case 0x4000:
-        
+        if(registers[x] != instruction & 0x00FF){
+            PC+=4;
+        }
+        else{
+            PC+=2;
+        }
         break;
 
         case 0x5000:
-        
+        if(registers[x] == registers[y]){
+            PC+=4;
+        }
+        else{
+            PC+=2;
+        }
         break;
 
         case 0x6000:
-        
+        registers[x] = instruction & 0x00FF;
+        PC+=2;
         break;
 
         case 0x7000:
-        
+        registers[x] += instruction & 0x00FF;
+        PC+=2;
         break;
 
         case 0x8000:
-        
+        switch(instruction & 0x000F){
+            case 0x0000:
+            registers[x] = registers[y];
+            PC+=2;
+            break;
+
+            case 0x0001:
+            registers[x] = registers[x] | registers[y];
+            PC+=2;
+            break;
+
+            case 0x0002:
+            registers[x] = registers[x] & registers[y];
+            PC+=2;
+            break;
+
+            case 0x0003:
+            registers[x] = registers[x] ^ registers[y];
+            PC+=2;
+            break;
+
+            case 0x0004:
+            registers[x] = registers[x] + registers[y];
+            if(registers[x] + registers[y] > 255){
+                registers[VF] = 1;
+            }
+            else{
+                registers[VF] = 0;
+            }
+            PC+=2;
+            break;
+
+            case 0x0005:
+            registers[x] = registers[x] - registers[y];
+            if(registers[x] > registers[y]){
+                registers[VF] = 1;
+            }
+            else{
+                registers[VF] = 0;
+            }
+            PC+=2;
+            break;
+
+            case 0x0006:
+            registers[VF] = registers[x] & 0x01;
+            registers[x] /= 2;
+            PC+=2;
+            break;
+
+            case 0x0007:
+            registers[x] = registers[y] - registers[x];
+            if(registers[y] > registers[x]){
+                registers[VF] = 1;
+            }
+            else{
+                registers[VF] = 0;
+            }
+            PC+=2;
+            break;
+
+            case 0x000E:
+            registers[VF] = registers[x] & 0x01;
+            registers[x] *= 2;
+            PC+=2;
+            break;
+
+            default:
+            printf("Instruction not handled: 0x%.4x\n");
+        }
         break;
 
         case 0x9000:
-        
+        switch(instruction & 0x0001){
+            case 0x0000:
+            if(registers[y] != registers[x]){
+                PC+=4;
+            }
+            else{
+                PC+=2;
+            }
+            break;
+
+            default:
+            printf("Instruction not handled: 0x%.4x\n");
+        }
         break;
 
         case 0xA000:
-        
+        I = instruction & 0x0FFF;
+        PC += 2;
         break;
 
         case 0xB000:
-        
+        PC = (instruction & 0x0FFF) + registers[V0];
         break;
 
         case 0xC000:
-        
+        srand(time(NULL));
+        uint32_t randomNumber = rand() % 256;
+        registers[x] = randomNumber & (instruction & 0x00FF);
         break;
 
         case 0xD000:
-        
+        int totalBytesToDisplay = instruction & 0x000F;
+        for(int i = 0; i < totalBytesToDisplay; i++){
+            
+        }
         break;
 
         case 0xE000:
@@ -207,6 +325,7 @@ int main(){
         printf("Instruction doesn't exist\n");
 
     }
+
     return 0;
 }
 
