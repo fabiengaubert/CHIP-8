9d561465da1595a9f1007e5ad7949106d28a62c8
FabienGaubert <fab.gaubert@gmail.com>
2021-03-26 13:11:31 +0100

    starting to implement instructions

--- main.c
+++ main.c
@@ -8,6 +8,8 @@
 #define SCREEN_WIDTH 64
 #define SCREEN_HEIGHT 32
 
+#define FONTS_SIZE 80
+
 const char* path="seven.ch8";
 
 uint8_t memory[MEMORY_SIZE];
@@ -32,6 +34,26 @@ enum RegistersNames{
     REGISTER_COUNT
 };
 
+uint8_t fonts[FONTS_SIZE] =
+{
+	0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+	0x20, 0x60, 0x20, 0x20, 0x70, // 1
+	0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+	0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+	0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+	0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+	0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+	0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+	0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+	0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+	0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+	0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+	0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+	0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+	0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+	0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+};
+
 uint8_t registers[REGISTER_COUNT];
 uint16_t PC=0x200;
 uint16_t I;
@@ -41,21 +63,8 @@ uint8_t soundTimer=0;
 // can be optimized with bools or bit operations
 unsigned char screen[SCREEN_HEIGHT][SCREEN_WIDTH];
 
-uint8_t stack[32];
+uint16_t stack[32];
 uint8_t SP=0x00;
-
-void init(){
-    //init registers to 0
-    for(int i=0; i<REGISTER_COUNT; i++){
-        registers[i]=0;
-    }
-    //init screen for test
-    for(int i=0; i<SCREEN_HEIGHT; i++){
-        for(int j=0; j<SCREEN_WIDTH; j++){
-            screen[i][j]='.';
-        }
-    }
-}
 
 void printScreen(){
     for(int i=0; i<SCREEN_HEIGHT; i++){
@@ -63,6 +72,14 @@ void printScreen(){
             printf("%c", screen[i][j]);
         }
         printf("\n");
+    }
+}
+
+void clearScreen(){
+    for(int i=0; i<SCREEN_HEIGHT; i++){
+        for(int j=0; j<SCREEN_WIDTH; j++){
+            screen[i][j]='.';
+        }
     }
 }
 
@@ -74,29 +91,131 @@ void printRAM(uint16_t adressStart, int number){
 
 int loadFile(const char* path){
     FILE* fp=fopen(path, "rb");
-    if(!fp){
+    if(fp==NULL){
         printf("erreur\n");
-        return 1;
+        return 0;
     }
     size_t numberBytesRead = fread((uint8_t*) memory+START_OF_PROGRAM, sizeof(uint8_t), MEMORY_SIZE-START_OF_PROGRAM, fp);
+    printf("%d bytes are loaded in memory\n", numberBytesRead);
     fclose(fp);
-    return 0;
+    return numberBytesRead;
 }
 
-void test(){
+void init(){
+    //init registers to 0
+    for(int i=0; i<REGISTER_COUNT; i++){
+        registers[i]=0;
+    }
+    clearScreen();
     //init RAM to 0 for test
     for(int i=0; i<MEMORY_SIZE; i++){
         memory[i]=0x00;
     }
 }
 
+void loadFonts(){
+    for(int i=0; i<FONTS_SIZE; i++){
+        memory[START_OF_FONT+i]=fonts[i];
+    }
+}
+
+
+
+
+
 int main(){
     init();
-    screen[16][32]='X';
-    screen[15][32]='X';
-    test();
-    loadFile(path);
+    int numberBytes=loadFile(path);
+    loadFonts();
+
     //printScreen();
-    printRAM(0x200, 14);
+    printRAM(0x200, numberBytes);
+
+
+    uint16_t instruction=memory[PC];
+    switch(instruction&0xF000){
+        case 0x0000:
+        switch(instruction){
+            case 0x00E0:
+            clearScreen();
+            break;
+        }
+        
+        break;
+
+        case 0x1000:
+        
+        break;
+
+        case 0x2000:
+        
+        break;
+
+        case 0x3000:
+        
+        break;
+
+        case 0x4000:
+        
+        break;
+
+        case 0x5000:
+        
+        break;
+
+        case 0x6000:
+        
+        break;
+
+        case 0x7000:
+        
+        break;
+
+        case 0x8000:
+        
+        break;
+
+        case 0x9000:
+        
+        break;
+
+        case 0xA000:
+        
+        break;
+
+        case 0xB000:
+        
+        break;
+
+        case 0xC000:
+        
+        break;
+
+        case 0xD000:
+        
+        break;
+
+        case 0xE000:
+        
+        break;
+
+        case 0xF000:
+        
+        break;
+
+        default:
+        printf("Instruction doesn't exist\n");
+
+    }
     return 0;
 }
+
+
+/*
+00e0 	clear the screen
+661e 	put the hex number 1e into register 6 ... the X screen coordinate is now 30 decimal
+670a 	put the hex number 0a into register 7 ... the Y screen coordinate is now 10 decimal
+6807 	put the hex number 7 into register 8 ... this is the number we want to display
+f829 	point the index register at the graphic representing the number in register 8
+d675 	display 5 graphic lines (i.e. the 7 sprite) at the location stored in registers 6 and 7
+f000*/
